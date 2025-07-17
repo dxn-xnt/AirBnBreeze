12171b1fe14cced4cd269a18a8e9888e107a7007
edmark1434 <edmarktalingting0@gmail.com>
2025-07-18 04:26:57 +0800

    docker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js (✔️ correct)
+module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
